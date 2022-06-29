85ec767579b2349e07aa472ae873176d68dd72aa
Adwaita Raj Modak <adwaitaraj@gmail.com>
2022-06-29 11:17:35 +0530

    Structuring Done

---
+++ server/Models/ChatModels.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const ChatSchema = new mongoose.Schema(
+    {
+        // user: {
+        //     type: mongoose.Schema.Types.ObjectId,
+        //     ref: 'User',
+        //     required: true
+        // },
+        // message: {
+        //     type: String,
+        //     required: true
+        // }
+        members: {
+            type: Array,
+        },
+    },
+    { timestamps: true }
+);
+
+const ChatModel = mongoose.model("Chat", ChatModel);
